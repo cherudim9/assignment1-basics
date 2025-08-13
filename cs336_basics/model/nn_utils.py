0a7abfbdd7b376c2fc5c8f404e3804df63354082
cherudim9 <cherudim9@gmail.com>
2025-08-13 15:28:39 -0700

    add softmax

--- cs336_basics/model/nn_utils.py
+++ cs336_basics/model/nn_utils.py
@@ -91,6 +91,13 @@ def silu(x: torch.Tensor):
     return x * torch.sigmoid(x)
 
 
+def softmax(x: torch.Tensor, dim: int):
+    x = x - torch.max(x, dim=dim, keepdim=True).values
+    x = torch.exp(x)
+    x = x / torch.sum(x, dim=dim, keepdim=True)
+    return x
+
+
 class Swiglu(nn.Module):
     def __init__(
         self,
--- tests/adapters.py
+++ tests/adapters.py
@@ -6,7 +6,7 @@ from collections.abc import Iterable
 from jaxtyping import Float, Int
 from cs336_basics.bpe import Bpe
 from cs336_basics.bpe_tokenizer import BpeTokenizer
-from cs336_basics.model.nn_utils import Linear, Embedding, RmsNorm, silu, Swiglu, Rope
+from cs336_basics.model.nn_utils import Linear, Embedding, RmsNorm, silu, Swiglu, Rope, softmax
 
 import numpy.typing as npt
 import torch
@@ -449,7 +449,7 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    return softmax(in_features, dim)
 
 
 def run_cross_entropy(
